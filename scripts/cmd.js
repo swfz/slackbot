5df65dd904ae59cfa8ef582a7959c569dc2f75fd
swfz <sawafuji.09@gmail.com>
2016-12-05 23:09:40 +0900

    update cmd

--- scripts/cmd.js
+++ scripts/cmd.js
@@ -1,20 +1,20 @@
 const Exec = require('child_process').exec;
-  controller.hears('cmd (.*)',['direct_message','direct_mention','mention'],(bot,message) => {
-    controller.storage.users.get(message.user,(err,data) => {
-      if (err){console.log(err);return}
-      else if ( !data.role || data.role != 'admin' ) {
-        bot.reply(message,'permission denied.');
-        return;
-      }
-      Exec(message.match[1], (err,stdout,stderr) => {
-        controller.log(`${message.user} : ${message.match[1]} \n STDERR: ${err} \n STDOUT: ${stdout}`);
+controller.hears('cmd (.*)',['direct_message','direct_mention','mention'],(bot,message) => {
+  controller.storage.users.get(message.user,(err,data) => {
+    if (err){console.log(err);return}
+    else if ( !data.role || data.role != 'admin' ) {
+      bot.reply(message,'permission denied.');
+      return;
+    }
+    Exec(message.match[1], (err,stdout,stderr) => {
+      controller.log(`${message.user} : ${message.match[1]} \n STDERR: ${err} \n STDOUT: ${stdout}`);
 
-        if ( err ) {
-          bot.reply(message,`Error!!!\n${err}`);
-        }
-        else {
-          bot.reply(message,`\`\`\`${stdout}\`\`\``);
-        }
-      });
+      if ( err ) {
+        bot.reply(message,`Error!!!\n${err}`);
+      }
+      else {
+        bot.reply(message,`\`\`\`${stdout}\`\`\``);
+      }
     });
   });
+});
